31641f6c7d692eef3f0f927aadfa864cd2263d5c
scul0405 <90521402+scul0405@users.noreply.github.com>
2023-09-24 14:08:08 +0700

    SE-3 [feature] Add mongoDB connection

--- server/index.js
+++ server/index.js
@@ -1,9 +1,24 @@
 const dotenv = require("dotenv");
+const mongoose = require("mongoose");
 const app = require("./app");
 
 dotenv.config({ path: "./config.env" });
 
 const port = process.env.PORT || 5000;
+
+// Database connection
+const DB_URI = process.env.MONGODB_URI.replace(
+  "<password>",
+  process.env.MONGODB_PASSWORD,
+);
+
+mongoose
+  .connect(DB_URI, {
+    useNewUrlParser: true,
+  })
+  .then(() => {
+    console.log("Connect to database successful !");
+  });
 
 app.listen(port, () => {
   console.log(`App running on port ${port} ...`);
