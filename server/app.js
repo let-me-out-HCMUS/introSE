79ce6a391240e3239902a014c4ece57823bab74e
scul0405 <90521402+scul0405@users.noreply.github.com>
2023-09-24 14:20:21 +0700

    SE-3 [feature] update CORS for client access

--- server/app.js
+++ server/app.js
@@ -40,7 +40,12 @@ if (process.env.NODE_ENV === "development") app.use(morgan("dev"));
 
 // middleware
 app.use(express.json());
-app.use(cors());
+app.use(
+  cors({
+    origin: ["http://localhost:3000", "http://localhost:3001"],
+    credentials: true,
+  }),
+);
 
 // Handle when no match any routes
 app.all("*", (req, res, next) =>
