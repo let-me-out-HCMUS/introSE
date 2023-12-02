9c1eaf2a395f6292c7c58b23630a0957e041f3f4
21120530 <phucmapcaumieu@gmail.com>
2023-12-02 10:12:42 +0700

    [fuc] delete protected route, refactor AppLayout

--- client/admin/src/App.jsx
+++ client/admin/src/App.jsx
@@ -6,13 +6,10 @@ import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import PageNotFound from "./pages/PageNotFound";
 import Home from "./pages/Home";
 
-import ProtectedRoute from "./ui/ProtectedRoute";
 import AppLayout from "./ui/AppLayout";
 import RankPage from "./pages/RankPage";
 import PlayerPage from "./pages/PlayerPage";
-import { Navbar } from "./features/navbar/Navbar";
 import DossierRegistration from "./pages/DossierRegistration";
-
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -26,53 +23,43 @@ function App() {
   return (
     <QueryClientProvider client={queryClient}>
       <ReactQueryDevtools initialIsOpen={false} />
-      <div className="container mx-auto p-5" style={{background: "#e5e5e5"}}>
-        <BrowserRouter>
-          <Navbar />
-          <Routes>
-            <Route
-              element={
-                <ProtectedRoute>
-                  <AppLayout />
-                </ProtectedRoute>
-              }
-            >
-              {/* Some route page that just use for admin */}
-            </Route>
 
-          <Route path="/" element={<Home />} />
-          <Route path="/rank" element={<RankPage />} />
-          <Route path="/register" element={<DossierRegistration />} />
-          <Route path="/players" element={<PlayerPage />} />
+      <BrowserRouter>
+        <Routes>
+          <Route element={<AppLayout />}>
+            <Route path="/" element={<Home />} />
+            <Route path="/rank" element={<RankPage />} />
+            <Route path="/register" element={<DossierRegistration />} />
+            <Route path="/players" element={<PlayerPage />} />
             {/* Another route add from here */}
 
             {/* All invalid route will render PageNotFound page */}
             <Route path="*" element={<PageNotFound />} />
-          </Routes>
-        </BrowserRouter>
+          </Route>
+        </Routes>
+      </BrowserRouter>
 
-        {/* Use for notification */}
-        <Toaster
-          position="top-center"
-          gutter={12}
-          containerStyle={{ margin: "8px" }}
-          toastOptions={{
-            success: {
-              duration: 3000,
-            },
-            error: {
-              duration: 5000,
-            },
-            style: {
-              fontSize: "16px",
-              maxWidth: "500px",
-              padding: "16px 24px",
-              backgroundColor: "var(--color-grey-0)",
-              color: "var(--color-grey-700)",
-            },
-          }}
-        />
-      </div>
+      {/* Use for notification */}
+      <Toaster
+        position="top-center"
+        gutter={12}
+        containerStyle={{ margin: "8px" }}
+        toastOptions={{
+          success: {
+            duration: 3000,
+          },
+          error: {
+            duration: 5000,
+          },
+          style: {
+            fontSize: "16px",
+            maxWidth: "500px",
+            padding: "16px 24px",
+            backgroundColor: "var(--color-grey-0)",
+            color: "var(--color-grey-700)",
+          },
+        }}
+      />
     </QueryClientProvider>
   );
 }
--- client/admin/src/ui/AppLayout.jsx
+++ client/admin/src/ui/AppLayout.jsx
@@ -1,10 +1,15 @@
 import { Outlet } from "react-router-dom";
+import { Navbar } from "./Navbar";
 
 function AppLayout() {
   // Config style for main app
   return (
-    <div>
+    <div className="containe mx-auto p-5" style={{ background: "#e5e5e5" }}>
+      <Navbar />
+
       <Outlet />
+
+      {/* Footer */}
     </div>
   );
 }
